c9a357bd3df69336e503c7b44c580a5ab9698f16
Gael MATUMONA <gaelmatumona2005@gmail.com>
2026-07-18 12:13:24 +0200

    Configuration SECRET_KEY pour Render

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(qt1t0=g8&u#31nb294e!6y%s*p!sz#l0+zvb2%h3qexj%m!ft'
+import os
+SECRET_KEY = SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
