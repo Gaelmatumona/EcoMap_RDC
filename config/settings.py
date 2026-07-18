54c1b852506bc7695b60e4c4227c58255919663a
Gael MATUMONA <gaelmatumona2005@gmail.com>
2026-07-18 11:10:06 +0200

    Preparation hebergement Django

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(qt1t0=g8&u#31nb294e!6y%s*p!sz#l0+zvb2%h3qexj%m!ft'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -134,3 +134,5 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
