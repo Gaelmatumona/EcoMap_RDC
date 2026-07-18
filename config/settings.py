19b7b55be4534e3125ef7ce2ad366bf67de34687
Gael MATUMONA <gaelmatumona2005@gmail.com>
2026-07-18 13:27:44 +0200

    Correction des fichiers statiques

--- config/settings.py
+++ config/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.whiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,9 +132,10 @@ AUTHENTICATION_BACKENDS = [
     "accounts.backends.CustomAuthBackend",
 ]
 
-STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
