7eaa3f6b41111ba9f7d34478c0db3600bc716c5a
Gael MATUMONA <gaelmatumona2005@gmail.com>
2026-07-23 00:41:34 +0200

    Ajout des configurations

--- config/settings.py
+++ config/settings.py
@@ -20,15 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# import os
-# SECRET_KEY = SECRET_KEY = os.environ.get("SECRET_KEY")
+import os
+SECRET_KEY = SECRET_KEY = os.environ.get("SECRET_KEY")
 
-SECRET_KEY = "django-insecure-cle-de-test-pour-le-developpement"
+# SECRET_KEY = "django-insecure-cle-de-test-pour-le-developpement"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
